2d8d62cc57e3ca792cced73e9dc311e816810929
z0f0237 <zaiba.fathima@walmart.com>
2026-06-18 17:24:55 -0500

    Initializing SpringBoot backend

---
+++ src/main/java/com/skypath/FlightConnectionSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.skypath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightConnectionSearchEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightConnectionSearchEngineApplication.class, args);
+    }
+
+}
